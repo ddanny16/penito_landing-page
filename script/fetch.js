2d931daadc656ff5f05780a4d1fdca189dd275ef
ddanny16 <dammydan16@gmail.com>
2022-06-30 13:34:07 +0200

    ...

--- script/fetch.js
+++ script/fetch.js
@@ -1,10 +1,28 @@
-function createNode(element){
-    return document.createElement(element)
+function createNode(element) {
+  return document.createElement(element);
 }
-function append (parent, el){
-    return parent.appendChild(el)
+function append(parent, el) {
+  return parent.appendChild(el);
 }
-const div = document.getElementById("demo");
-let url = "https://fakestoreapi.com/products?limit=10";
+const div = document.getElementById("items");
+const url = "https://fakestoreapi.com/products?limit=10";
 
-const createNode = (element)
+fetch(url)
+  .then((resp) => resp.json())
+  .then(function (data) {
+    console.log(data)
+    let items = data;
+    return items.map(function (item) {
+      let li = createNode("li");
+      img = createNode("img");
+      span = createNode("span");
+      img.src = item.image;
+      span.innerHTML = `${item.title} ${item.category} ${item.price}`;
+      append(li, img)
+      append(li, span)
+      append(div, li)
+    });
+  })
+  .catch(function(error){
+    console.log(error)
+  })
